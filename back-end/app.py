1a1dcd3e834be27f595f60cb4ffab8a6061e6b12
Nageswari <nageswaribora50@gmail.com>
2026-05-15 17:07:57 +0530

    fixed routes

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {
+    return jsonify({
         "message": "NutraAI Backend Running Successfully"
-    }
+    })
+
+@app.route('/review')
+def review():
+    return jsonify({
+        "status": "Review API Working"
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
